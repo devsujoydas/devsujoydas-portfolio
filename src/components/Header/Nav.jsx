0c51806334ee424a950ef9d88707dbd5edd6a8d2
devsujoydas <devsujoydas@gmail.com>
2025-04-29 15:39:44 +0600

    delete resume from the navbvar

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -8,7 +8,6 @@ const Nav = () => {
             <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#service">Services</a>
             <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#projects">Projects</a>
             <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#skills">Skills</a>
-            <a className="hover:text-[#FF014F] duration-500 transition-all" target='_blank' href="https://drive.google.com/file/d/12yAP7YIjhRR7SZVXFcGI3tg3Cjt80Pse/view?usp=drive_link">Resume</a>
             <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#contact">Contact</a>
           </ul>
     </div>
