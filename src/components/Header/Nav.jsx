dcd146719aecba84df4771a95be709ce7be7813e
devsujoydas <devsujoydas@gmail.com>
2025-06-18 01:56:19 +0600

    update navbar

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className="flex md:flex-row flex-col justify-center md:items-center flex-wrap md:gap-10 gap-3 md:text-lg">
+    <div className="flex md:flex-row flex-col justify-center md:items-center flex-wrap md:gap-10 gap-4 md:text-lg">
       <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#home">Home</a>
       <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#service">Services</a>
       <a className="hover:text-[#FF014F] duration-500 transition-all" href="/#projects">Projects</a>
